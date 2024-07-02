c7d7b7395c09421524937aae3971bec425b8a4d7
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-02 16:37:18 +0530

    src: user_input

---
+++ src/007_user_input.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  /* User Input */
+
+  int age;
+
+  printf("How old are you??\n>> ");
+  scanf("%d", &age);
+
+  printf("\nAge: %d", age);
+
+  return 0;
+}
+
+
