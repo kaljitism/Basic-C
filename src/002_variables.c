8c047d5595e96bcba7eef30d606d98b183d23e33
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-02 16:36:08 +0530

    src: variables

---
+++ src/002_variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+    // Variables
+
+    int x; // Declaration
+    x = 123; // Initialization
+    int y = 321; // Declaration + Initialization
+
+    int age = 21; // int
+    float gpa = 2.05;  // floating point
+    char grade = 'C'; // string character
+    char name[] = "Aditya"; // array of characters
+
+    printf("Hello %s\n", name);
+    printf("You are %d years old\n", age);
+    printf("You got %c in test\n", grade);
+}
