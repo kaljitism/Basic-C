4eef928c41d0c62bc5761f73c3703a87183f1c3d
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-02 16:35:32 +0530

    src: comments

---
+++ src/001_comments_and_escape_sequences.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    /* This is a multiline comment
+   * This
+   * is
+   * a
+   * multiline
+   * comment
+   */
+    printf("C is reallyy goood!!\n");
+    printf("-\"me\"\n");
+
+    return 0;
+}
