a893a823504b518bbec280a570892d3999c63860
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-04 23:49:34 +0530

    src: do while loop

---
+++ src/020_dowhile_loop.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+  int number = 0;
+  int sum = 0;
+
+  do {
+    printf("Enter a number above 0: >> ");
+    scanf("%d", &number);
+
+    if (number > 0) {
+      sum += number;
+    }
+  } while( number > 0 );
+
+  printf("%d\n", sum);
+}
