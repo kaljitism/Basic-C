3af8319206ca33ea99b2309fa8c52147ee6da870
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-04 23:49:02 +0530

    src: string functions

---
+++ src/017_string_functions.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+  char string1[] = "Foo";
+  char string2[] = "Bar";
+
+  // strcat(string1, string2);  // concatenate
+  // strncat(string1, string2, 1);  // concatenate n characters from string2
+  // strcpy(string1, string2);  // copy string2 to string1
+  // strncpy(string1, string2, 2); // copy n characters of string2 to string1 
+  
+  // int result = strlen(string1);  // returns string length as int
+  // int result = strcmp(string1, string2);  // compares all characters.
+  // int result = strncmp(string1, string2, 1);  // compare n characters.
+
+  // printf("%s\n", string1);
+  printf("%d\n", result);
+}
