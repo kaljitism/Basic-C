4c51231ccaa3118b3bdf6201739993e98621ae9a
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-04 23:50:01 +0530

    src: break continue statement

---
+++ src/022_break_continue.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main() {
+  int i = 0;
+
+  while ( true ) {
+    i++;
+    if ( i % 3 == 0 && i % 5 != 0 ) {
+      printf("%d => fizz\n", i);
+      continue;
+    } 
+    if ( i % 5 == 0 && i % 3 != 0 ) {
+      printf("%d => buzz\n", i);
+      continue;
+    } 
+    if ( i % 15 == 0 ) {
+      printf("%d => fizzbuzz\n", i);
+      continue;
+    }
+    if ( i > 100 ) {
+      break;
+    }
+  }
+}
