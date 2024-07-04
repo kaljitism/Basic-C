1ba20274332f36f0106c19bca3091c00903e5b24
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-04 23:46:34 +0530

    src: append return 0; in main()

--- src/007_user_input.c
+++ src/007_user_input.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   /* User Input */
 
+  char name[25]; // bytes
   int age;
+  
+  // scanf() reads till first whitespace
+  // to include whitespaces, use fgets()
+  // by default, when you hit enter in fgets, it includes \n in the input
+
+  printf("What is your name??\n>> ");
+  // scanf("%s", &name);   
+  fgets(name, 25, stdin);
+  name[strlen(name) - 1] = '\0';
 
   printf("How old are you??\n>> ");
   scanf("%d", &age);
 
-  printf("\nAge: %d", age);
+  printf("\n --- USER DETAILS --- \nName: %s\nAge: %d\n", name, age);
 
   return 0;
 }
