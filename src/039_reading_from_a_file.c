468fadcb06b87bd91176d568f65b235d12dd420f
Aditya Dwivedi <dwikaljit@gmail.com>
2024-07-05 21:27:34 +0530

    src: reading from a file

---
+++ src/039_reading_from_a_file.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+  FILE *pFile = fopen("./poem.txt", "r");
+  char buffer[255];
+  // fgets(buffer, 255, pFile);  // read one line - 255 bytes/ 255 characters
+  // printf("%s\n", buffer);
+
+  if (pFile == NULL) {
+    printf("Unable to open file!\n");
+  } else {
+
+  while (fgets(buffer, 255, pFile) != NULL) {   
+    printf("%s", buffer);
+  }
+
+  fclose(pFile);
+
+  return 0;
+}
